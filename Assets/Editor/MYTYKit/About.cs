fa01cd01cc37b7ef5f91b11944123c9816ec46da
GW <gw@off.live>
2022-08-18 23:27:46 +0900

    feat : about windows and version info

--- Assets/Editor/MYTYKit/About.cs
+++ Assets/Editor/MYTYKit/About.cs
@@ -1,5 +1,6 @@
 using System.Collections;
 using System.Collections.Generic;
+using System.IO;
 using UnityEngine;
 using UnityEditor;
 using UnityEditor.UIElements;
@@ -7,10 +8,26 @@ using UnityEngine.UIElements;
 
 public class About : EditorWindow
 {
+    public VisualTreeAsset UITemplate;
+
     [MenuItem("MYTY Kit/About", false, 100)]
     public static void ShowWindow()
     {
         var wnd = GetWindow<About>();
         wnd.titleContent = new GUIContent("About");
+    }
+
+    void CreateGUI()
+    {
+        UITemplate.CloneTree(rootVisualElement);
+        maxSize = new Vector2(300, 120);
+        minSize = maxSize;
+
+        var versionField = rootVisualElement.Q<Label>("LBLVersion");
+        var filename = Application.streamingAssetsPath + "/VERSION.txt";
+        var versionStr = File.ReadAllText(filename);
+        versionStr = versionStr.Trim();
+        versionField.text = versionStr;
+
     }
 }
