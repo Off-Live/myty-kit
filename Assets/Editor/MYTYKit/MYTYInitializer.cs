8c7a80f97a7b20e983e46e5b1287e2f767055cbc
GW <gw@off.live>
2022-10-26 09:52:41 +0900

    fix : install bug fixing

--- Assets/Editor/MYTYKit/MYTYInitializer.cs
+++ Assets/Editor/MYTYKit/MYTYInitializer.cs
@@ -15,13 +15,19 @@ namespace MYTYKit
         {
             Debug.Log("MYTYKit Start Up");
 
-            // var testPath = Path.GetFullPath("Packages/com.unity.2d.common");
-            // var files = Directory.GetFiles(testPath);
-            // foreach (var file in files)
+            var testPath = Path.GetFullPath("Packages/com.unity.2d.common");
+            var files = Directory.GetFiles(testPath);
+            // foreach (var dir in files)
             // {
-            //     FileInfo fileinfo = new FileInfo(file);
-            //     Debug.Log(fileinfo.Name);
+            //     var info = new DirectoryInfo(dir);
+            //     Debug.Log(dir);
+            //     Debug.Log(info.Name);
             // }
+            foreach (var file in files)
+            {
+                FileInfo fileinfo = new FileInfo(file);
+                Debug.Log(fileinfo.Name + "  "+fileinfo.Extension);
+            }
             var kitFullPath = Path.GetFullPath(KitPath);
             if (!Directory.Exists(kitFullPath)) return;
             CopyKitFiles();
@@ -32,30 +38,41 @@ namespace MYTYKit
         {
             var kitAssetPath = "Assets/MYTYKit";
             var kitFullPath = Path.GetFullPath(KitPath);
-            //if (Directory.Exists(assetPath)) return;
-            FileUtil.CopyFileOrDirectory(kitFullPath,kitAssetPath);
+            var subAssetDir = new string[]
+            {
+                "CmdTools","LayerEffect","MotionAdapter","MotionTemplate"
+            };
+
+            foreach (var dir in subAssetDir)
+            {
+                RecursiveCopy(kitFullPath+"/"+dir, kitAssetPath+"/"+dir);
+            }
         }
 
         static void CopyStreamingAssets()
         {
             var saFullPath = Path.GetFullPath(StreamingAssetsPath);
-            var files = Directory.GetFiles(saFullPath);
+            RecursiveCopy(saFullPath, StreamingAssetsPath);
+        }
+
+        static void RecursiveCopy(string fromAbsolutePath, string toPath)
+        {
+            var files = Directory.GetFiles(fromAbsolutePath);
             foreach (var file in files)
             {
-                var info = new FileInfo(file);
-                var targetPath = Application.streamingAssetsPath + "/" + info.Name;
-                if (!File.Exists(targetPath))
-                {
-                    FileUtil.CopyFileOrDirectory(file, targetPath);
-                }
-            }
-
-            var mytySaPath = Application.streamingAssetsPath + "/MYTYKit";
-            if (!Directory.Exists(mytySaPath))
-            {
-                FileUtil.CopyFileOrDirectory(StreamingAssetsPath+"/MYTYKit",mytySaPath );
+                var fileInfo = new FileInfo(file);
+                var toFile = toPath + "/" + fileInfo.Name;
+                if (fileInfo.Extension == ".meta") continue;
+                if (File.Exists(toFile)) continue;
+                FileUtil.CopyFileOrDirectory(file, toFile);
             }
             
+            var subdirs = Directory.GetDirectories(fromAbsolutePath);
+            foreach (var subdir in subdirs)
+            {
+                var dirInfo = new DirectoryInfo(subdir);
+                RecursiveCopy(subdir,toPath+"/"+dirInfo.Name);
+            }
         }
     }
 }
